0e5536a2a58e7d3fdbcde8914b5606b46051043c
Reese Morgan <ramorgan212@gmail.com>
2020-10-10 16:10:53 -0400

    move three col layout to the layout blade file.

--- web/resources/views/tweets/index.blade.php
+++ web/resources/views/tweets/index.blade.php
@@ -1,27 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="lg:flex lg:justify-between">
-        <div class="lg:w-32">
-            @include('_sidebar-links')
-        </div>
+    @include ('_publish-tweet-panel')
 
-
-        <div class="flex-1 lg:mx-10" style="max-width:700px">
-            @include('_publish_tweet_panel')
-
-
-            <div id="time-line" class="border border-gray-300 rounded-lg">
-                @foreach($tweets as $tweet)
-                    @include('_tweet')
-                @endforeach
-            </div>
-
-        </div>
-
-
-        <div class="lg:w-1/6 bg-blue-100 rounded-large p-4">
-            @include('_follows_list')
-        </div>
-    </div>
+    @include ('_timeline')
 @endsection
