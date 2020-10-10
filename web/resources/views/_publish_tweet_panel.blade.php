1b5255a4d24a572739c46c5faf7f259f1545c719
Reese Morgan <ramorgan212@gmail.com>
2020-10-10 14:05:54 -0400

    update views to display dynamic info for tweet (fake timeline)

--- web/resources/views/_publish_tweet_panel.blade.php
+++ web/resources/views/_publish_tweet_panel.blade.php
@@ -1,21 +1,33 @@
 <div id="tweet-box" class="border border-blue-400 rounded-lg px-6 py-4 mb-8">
-    <form action="">
-                <textarea
-                    name="body"
-                    id="msg_body"
-                    placeholder="Whats up Doc?"
-                    class="w-full"
+    <form method='POST' action="/tweets">
+
+        @csrf
+
+        <textarea
+            name="body"
+            id="msg_body"
+            placeholder="Whats up Doc?"
+            class="w-full"
+{{--                    required--}}
                 ></textarea>
 
         <hr class="my-4"/>
 
         <footer class="flex justify-between">
             <img
-                src="https://api.adorable.io/avatars/40/tweety.png"
+                src="{{auth()->user()->avatar}}"
                 alt="avatar"
                 class="profile rounded-full mr-2"
             >
-            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Post message</button>
+            <button
+                type="submit"
+                class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white"
+            >Tweet-a-roo
+            </button>
         </footer>
     </form>
+
+    @error('body')
+    <p class="text-red-500 text-lg mt-2">{{$message}}</p>
+    @enderror
 </div>
