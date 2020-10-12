415ac3516ad9ba560e607d49a4818661482ba18e
Reese Morgan <ramorgan212@gmail.com>
2020-10-12 15:13:24 -0400

    increased the size for border radius large

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -194,7 +194,7 @@ module.exports = {
       sm: '0.125rem',
       default: '0.25rem',
       md: '0.375rem',
-      lg: '0.5rem',
+      lg: '1.25rem',
       full: '9999px',
     },
     borderWidth: {
