54959cd9644a819b408124358589878f79500f75
Reese Morgan <ramorgan212@gmail.com>
2020-10-12 15:55:28 -0400

    add laravel ide helper

--- web/app/Models/Tweet.php
+++ web/app/Models/Tweet.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Tweet
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $body
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read User $user
+ * @method static Builder|Tweet newModelQuery()
+ * @method static Builder|Tweet newQuery()
+ * @method static Builder|Tweet query()
+ * @method static Builder|Tweet whereBody($value)
+ * @method static Builder|Tweet whereCreatedAt($value)
+ * @method static Builder|Tweet whereId($value)
+ * @method static Builder|Tweet whereUpdatedAt($value)
+ * @method static Builder|Tweet whereUserId($value)
+ * @mixin Eloquent
+ */
 class Tweet extends Model
 {
     protected $fillable = [
--- web/app/Models/User.php
+++ web/app/Models/User.php
@@ -2,13 +2,50 @@
 
 namespace App\Models;
 
+use Eloquent;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection|User[] $follows
+ * @property-read int|null $follows_count
+ * @property-read mixed $avatar
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read Collection|Tweet[] $tweets
+ * @property-read int|null $tweets_count
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereEmailVerifiedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
