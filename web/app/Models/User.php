aebabf02e5a598058fad6d9bcedca47406b87137
Reese Morgan <ramorgan212@gmail.com>
2020-10-13 23:12:05 -0400

    user->path is appendable

--- web/app/Models/User.php
+++ web/app/Models/User.php
@@ -101,9 +101,10 @@ class User extends Authenticatable
         return $this->hasMany(Tweet::class)->latest();
     }
 
-    public function path()
+    public function path($append = '')
     {
-        return route('profile', $this);
+        $path = route('profile', $this->name);
+        return $append ? "{$path}/{$append}" : $path;
     }
 
 }
